6f2ecabeb3f647cc6d3deb39af755dad820a7f90
Lucky Amadi <lucky@softcom.ng>
2020-01-21 23:41:49 +0100

    typescript setup done

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const port = 8080;
+app.listen(port, () => console.log(`server running on port ${port}`));
+//# sourceMappingURL=index.js.map
