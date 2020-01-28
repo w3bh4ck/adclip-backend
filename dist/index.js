e0be73a8963e8d8ccca3bcbaee3b1b655b220bce
Lucky Amadi <lucky@softcom.ng>
2020-01-28 02:41:48 +0100

    successfuly connected to database with knex

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,28 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const apollo_server_1 = require("apollo-server");
+const knex_1 = __importDefault(require("knex"));
 const typeDefinition_1 = require("./typeDefinitions/typeDefinition");
+const connection = knex_1.default({
+    client: "pg",
+    connection: {
+        host: "127.0.0.1",
+        user: "w3bh4ck",
+        password: "secret",
+        database: "adclip"
+    }
+});
+const checkConnection = connection("users")
+    .where({
+    username: "lawkey"
+})
+    .select("email");
 const server = new apollo_server_1.ApolloServer({ typeDefs: typeDefinition_1.typeDefs, resolvers: typeDefinition_1.resolvers });
-const port = 8080;
 server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
+    console.log("from db", checkConnection);
 });
-// const port = 8080;
-// const app = express();
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(
-// 	"/graphql",
-// 	graphqlHTTP({
-// 		schema: null,
-// 		graphiql: true
-// 	})
-// );
-// app.use(dashboardRoute);
-// app.listen(port, () => console.log(`running on port ${port}`));
 //# sourceMappingURL=index.js.map
