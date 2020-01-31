2adea6132f2067e918c6bd7bc675c56db03311c1
Lucky Amadi <lucky@softcom.ng>
2020-01-31 23:26:15 +0100

    removed the rest implementation

--- src/routes/auth/loginRouter.ts
+++
@@ -1,1 +0,0 @@
-import { Router, Request } from "express";
--- src/routes/dashboard/dashboardRoute.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from "express";
-
-const dashboardRoute = Router();
-
-dashboardRoute.get("/", (req: Request, res: Response) => {
-	res.send("this is the dashboard router");
-});
-
-export { dashboardRoute };
