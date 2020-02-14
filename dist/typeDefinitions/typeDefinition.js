f926714f6ef570887c7d71bc1a2acced54929388
Lucky Amadi <lucky@softcom.ng>
2020-02-14 23:18:24 +0100

    started fixing typeorm errors

--- dist/typeDefinitions/typeDefinition.js
+++ dist/typeDefinitions/typeDefinition.js
@@ -1,13 +1,4 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -18,30 +9,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const typeorm_1 = require("typeorm");
 const dbConnection_1 = require("../db/dbConnection");
 const apollo_server_1 = require("apollo-server");
-let userProfile = class userProfile {
-};
-__decorate([
-    typeorm_1.PrimaryColumn("uuid"),
-    __metadata("design:type", String)
-], userProfile.prototype, "id", void 0);
-__decorate([
-    typeorm_1.Column("text"),
-    __metadata("design:type", String)
-], userProfile.prototype, "username", void 0);
-__decorate([
-    typeorm_1.Column("text"),
-    __metadata("design:type", String)
-], userProfile.prototype, "email", void 0);
-__decorate([
-    typeorm_1.Column(),
-    __metadata("design:type", String)
-], userProfile.prototype, "password", void 0);
-userProfile = __decorate([
-    typeorm_1.Entity()
-], userProfile);
+// interface newUser {
+// 	email: String;
+// 	username: String;
+// 	password: String;
+// 	id: number;
+// }
+// @Entity()
+// class userProfile {
+// 	@PrimaryColumn("uuid")
+// 	id: string;
+// 	@Column("text")
+// 	username: string;
+// 	@Column("text")
+// 	email: string;
+// 	@Column()
+// 	password: string;
+// }
 exports.typeDefs = apollo_server_1.gql `
 	type Users {
 		username: String
@@ -78,22 +64,22 @@ exports.resolvers = {
             const users = yield dbConnection_1.connection.select("*").from("users");
             return users;
         })
-    },
-    Mutation: {
-        addUser(_, { input }) {
-            typeorm_1.createConnection()
-                .then((connection) => __awaiter(this, void 0, void 0, function* () {
-                let newProfile = new userProfile();
-                newProfile.email = input.email;
-                newProfile.username = input.username;
-                newProfile.password = input.password;
-                yield connection.manager.save(newProfile);
-                console.log("Photo has been saved", newProfile);
-            }))
-                .catch(error => console.log(error));
-            return input;
-            // add knex function here
-        }
     }
+    // Mutation: {
+    // 	addUser(_: any, { input }: any) {
+    // 		createConnection()
+    // 			.then(async connection => {
+    // 				let newProfile = new userProfile();
+    // 				newProfile.email = input.email;
+    // 				newProfile.username = input.username;
+    // 				newProfile.password = input.password;
+    // 				await connection.manager.save(newProfile);
+    // 				console.log("Photo has been saved", newProfile);
+    // 			})
+    // 			.catch(error => console.log(error));
+    // 		return input;
+    // 		// add knex function here
+    // 	}
+    // }
 };
 //# sourceMappingURL=typeDefinition.js.map
