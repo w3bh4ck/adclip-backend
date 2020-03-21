9eade20a383fcd056cc880f67fdd641acf6006f0
Lucky Amadi <lucky@softcom.ng>
2020-03-21 03:55:05 +0100

    connecting to the backend with sequelize

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./types/typeDefinitions";
 import { resolvers } from "./resolvers/resolver";
-import { createConnection } from "typeorm";
+import { Sequelize, Model, DataTypes, BuildOptions } from "sequelize";
+
+const sequelize = new Sequelize("adclip", "w3bh4ck", "secret", {
+	host: "localhost",
+	dialect: "postgres"
+});
+
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log("Connection has been established successfully.");
+	})
+	.catch((err: object) => {
+		console.error("Unable to connect to the database:", err);
+	});
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-createConnection()
-	.then(async () => {
-		console.log("connection successful");
-		server.listen(4000).then(() => console.log("app is running on port 4000"));
-	})
-	.catch(error => console.log(error));
+server.listen(4000).then(() => console.log("app is running on port 4000"));
